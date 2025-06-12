74275958a3a62df2e8431cdd0a490ffa846f0693
Lalu Mahato <lalumahato020@gmail.com>
2025-06-12 19:44:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App () {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p>React with Vite</p>
+    </>
+  )
+}
+
+export default App
